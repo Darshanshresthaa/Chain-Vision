942859056b7481dfbbbb0d860d9fdb44332a9d0c
Darshan Shrestha <darshanshrestha332@gmail.com>
2026-05-12 15:33:17 +0545

    Complete Summary Generation

--- core/Summary.py
+++ core/Summary.py
@@ -32,6 +32,9 @@ def summarize_data(transcript: str) -> str:
 
     llm = get_llm()
 
+
+# GENERATE SUMMARY OF EVERY CHUNK
+
     summarize_prompt = ChatPromptTemplate.from_messages([
         (
             "system",
@@ -52,11 +55,37 @@ def summarize_data(transcript: str) -> str:
         )
     ])
 
-    summarize_chain = summarize_prompt | llm | parser
 
     chunk_list = chunk_text(transcript)
 
+     
+# GENERATE A TITLE OF TOPIC
+    title_prompt = ChatPromptTemplate.from_messages([
+    ("system",
+        """
+        You are an expert title generator.
+
+        Generate one clear, professional, and concise title
+        based on the provided transcript summary.
+
+        Instructions:
+        - Keep the title short and meaningful
+        - Capture the main topic accurately
+        - Make it easy to understand
+        - Avoid clickbait or overly dramatic wording
+        - Return only the title
+
+        """),
+
+    ("human","{summary}")
+    ])
+
+
+
+
     chunk_summary = []
+     # CHAIN -1  Chunk every transcipt ->list
+    summarize_chain = summarize_prompt | llm | parser
 
     for chunk in chunk_list:
 
@@ -67,6 +96,12 @@ def summarize_data(transcript: str) -> str:
         chunk_summary.append(summary_chunk)
 
     combined_summary = " ".join(chunk_summary)
+
+
+    # CHAIN -2 -> Generate TOPIC TITLE
+    chain_title = title_prompt | llm | parser
+
+    # title = chain_title.invoke({"summary":combined_summary})
 
     final_combine_summarize = PromptTemplate.from_template("""
     You are a helpful AI assistant.
@@ -83,18 +118,30 @@ def summarize_data(transcript: str) -> str:
     - Avoid repeating the same information
 
     Partial Summaries:
-    {summaries}
+    {summary}
 
     Final Summary:
     """)
 
     final_chain = final_combine_summarize | llm | parser
 
-    final_summary = final_chain.invoke({
-        "summaries": combined_summary
+    # final_summary = final_chain.invoke({
+    #     "summaries": combined_summary
+    # })
+
+    # return final_summary
+    
+    # Final Chain
+
+
+    final_parallel_chain = RunnableParallel({
+    "title": chain_title,
+    "summary": final_chain
     })
 
-    return final_summary
+    final_output = final_parallel_chain.invoke({"summary":combined_summary})
+
+    return final_output
 
 
 
